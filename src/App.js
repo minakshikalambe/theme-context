bb8348067c063a69f6b0b563ea8c1a16f368189c
minakshikalambe <97457265+minakshikalambe@users.noreply.github.com>
2022-04-24 14:37:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <span style={{ color: darkMode ? "grey" : "yellow" }}>light</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>dark</span>
+      </div>
+    
+    </div>
+  );
+}
+
